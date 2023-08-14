a9324c03919b034afccfa1f6e71ccdc54b65e364
saksham29008 <132270568+saksham29008@users.noreply.github.com>
2023-08-14 18:00:25 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,27 @@
+class Player {
+  /* constructor() {
+    this.name = null;
+    this.index = null;
+    this.positionX = 0;
+    this.positionY = 0;
+  }
+  getCount{}{
+    varPlayerCountRef=database.ref("playerref")
+
+  addPlayer() {
+
+    var playerIndex = "players/player" + this.index;
+
+    if (this.index === 1) {
+      this.positionX = width / 2 - 100;
+    } else {
+      this.positionX = width / 2 + 100;
+    }
+
+    database.ref(playerIndex).set({
+      name: this.name,
+      positionX: this.positionX,
+      positionY: this.positionY,
+    });
+  } */
+}
